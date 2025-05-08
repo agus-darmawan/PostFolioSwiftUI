9d5cac9e78b0ca33c10ecb735fcb077059b7783d
agus-darmawan <wayanagus.dr@gmail.com>
2025-05-08 10:50:09 +0700

    Initial Commit

---
+++ PostFolio/PostFolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostFolioApp.swift
+//  PostFolio
+//
+//  Created by Darmawan on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PostFolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
